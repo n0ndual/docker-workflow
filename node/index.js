d02c5c9f77ae43075a1b03115b3f6da95af9e73e
augustin.mars <augustin.programmer.mars@gamil.com>
2016-01-22 18:53:15 +0800

    redis not using temporarily

--- node/index.js
+++ node/index.js
@@ -13,15 +13,16 @@ console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_
 // );
 
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-var client = redis.createClient('6379', 'redis');
+//var client = redis.createClient('6379', 'redis');
 
 
 app.get('/', function(req, res, next) {
     ip = 'Dear friend from ' + req.query.ip +", Welcome to CDS GPN!\n";
-    client.incr('counter', function(err, counter) {
-      if(err) return next(err);
-      res.send(ip + 'This page has been viewed ' + counter + ' times!');
-    });
+    res.send(ip);
+//    client.incr('counter', function(err, counter) {
+//      if(err) return next(err);
+//      res.send(ip + 'This page has been viewed ' + counter + ' times!');
+//    });
 });
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
