842c398746b23509a23d60a9eca5c782ba76e366
augustin.mars <augustin.programmer.mars@gamil.com>
2016-01-23 23:41:36 +0800

    hah

--- node/index.js
+++ node/index.js
@@ -7,22 +7,29 @@ var app = express();
 console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT);
 
 // APPROACH 1: Using environment variables created by Docker
-// var client = redis.createClient(
+//var client = redis.createClient(
 // 	process.env.REDIS_PORT_6379_TCP_PORT,
 //   	process.env.REDIS_PORT_6379_TCP_ADDR
-// );
+//);
 
 // APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
-//var client = redis.createClient('6379', 'redis');
+var client1 = redis.createClient('6379', 'redis1');
+var client2 = redis.createClient('6379', 'redis2');
 
 
 app.get('/', function(req, res, next) {
     ip = 'Dear friend from ' + req.query.ip +", Welcome to CDS GPN!\n";
-    res.send(ip);
-//    client.incr('counter', function(err, counter) {
-//      if(err) return next(err);
-//      res.send(ip + 'This page has been viewed ' + counter + ' times!');
-//    });
+    if(req.query.ip=="US")  {
+	client1.incr('counter', function(err, counter) {
+	    if(err) return next(err);
+	    res.send(ip + 'This page has been viewed ' + counter + ' times!');
+	});	
+    }else{
+	client2.incr('counter', function(err, counter) {
+	    if(err) return next(err);
+	    res.send(ip + 'This page has been viewed ' + counter + ' times!');
+	});
+    };
 });
 
 http.createServer(app).listen(process.env.PORT || 8080, function() {
--- node/start.sh
+++ node/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-while ! nc -z redis 6379; do sleep 3; done
+while ! nc -z redis1 6379; do sleep 3; done
+while ! nc -z redis2 6379; do sleep 3; done
 nodemon /src/index.js
